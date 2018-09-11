ad9ae5011c9947ecd690faf5842730baac86d646
saskamegaprogrammist <40800678+saskamegaprogrammist@users.noreply.github.com>
2018-09-11 20:54:13 +0300

    Update taverna_new.js
    добавление препятствий

--- client/js/taverna_new.js
+++ client/js/taverna_new.js
@@ -24,7 +24,7 @@ let elf = {
 };
 
 function metElf(){
-    if ((hero.x <= elf.x2 && hero.x >=elf.x1) && (hero.y <= elf.y2 && hero.x >=elf.y1)){
+    if ((hero.x <= elf.x2 && hero.x >=elf.x1) && (hero.y <= elf.y2 && hero.y >=elf.y1)){ 
         return true;
     }
     return false;
@@ -59,8 +59,52 @@ function animation() {
 
         },  70)
 }
+let taverna_barrier_1 ={
+    x1: 475,
+    y1: 550,
+    x2: 600,
+    y2: 650
+};
 
+let taverna_barrier_2 ={
+    x1: 140,
+    y1: 310,
+    x2: 260,
+    y2: 410
+};
 
+let taverna_barrier_3 ={
+    x1: 0,
+    y1: 0,
+    x2: 384,
+    y2: 180
+};
 
+let taverna_barrier_4 ={
+    x1: 560,
+    y1: 0,
+    x2: 786,
+    y2: 384
+};
 
+let elf_obstacle={
+    x1: 230,
+    y1: 180,
+    x2: 310,
+    y2: 300
+};
 
+function collision_tavern(obstacle) {
+    if (hero.x==obstacle.x1 && (hero.y>=obstacle.y1 && hero.y <=obstacle.y2)){
+        hero.x-=10;
+    }
+    if (hero.x==obstacle.x2 && (hero.y>=obstacle.y1 && hero.y <=obstacle.y2)){
+        hero.x+=10;
+    }
+    if (hero.y == obstacle.y1 && (hero.x >= obstacle.x1 && hero.x<= obstacle.x2)){
+        hero.y-=10;
+    }
+    if (hero.y == obstacle.y2 && (hero.x >= obstacle.x1 && hero.x<= obstacle.x2)){
+        hero.y+=10;
+    }
+}
